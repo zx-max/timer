fc27659ed0fc063da95ed3ccb1e247526cb9b6ec
max <massimiliano.parentini@gmail.com>
2012-09-14 15:46:43 +0200

    added a System.exit(0); in order to  close the application.

--- src/main/java/max/utility/tomato/gui/window/listener/CloseTimersListener.java
+++ src/main/java/max/utility/tomato/gui/window/listener/CloseTimersListener.java
@@ -3,17 +3,11 @@ package max.utility.tomato.gui.window.listener;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.util.concurrent.Callable;
 
 import javax.swing.JFrame;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import max.utility.tomato.Main;
-import max.utility.tomato.gui.EndTomato;
-import max.utility.tomato.gui.StartTimer;
 
 public class CloseTimersListener extends  WindowAdapter {
 
@@ -28,14 +22,14 @@ public class CloseTimersListener extends  WindowAdapter {
 	public void windowClosing(WindowEvent arg0) {
 	logger.debug("start closing windows..");
 		for (int i = 0; i < jFrames.length; i++) {
+			jFrames[i].setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			WindowEvent wev = new WindowEvent(jFrames[i], WindowEvent.WINDOW_CLOSING);
 			Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(wev);
 			logger.debug(jFrames[i].getTitle());
-			jFrames[i].setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//			jFrames[i].setVisible(false);
-//			jFrames[i].dispose();
 		}
-
+		
+		//http://stackoverflow.com/questions/1234912/how-to-programmatically-close-a-jframe
+		System.exit(0); 
 	}
 
 
