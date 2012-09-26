9bd016fc3b02dcdace0ee10c41e19d224aca7214
max <massimiliano.parentini@gmail.com>
2012-09-26 13:55:49 +0200

    create the distrubution file zip fix: nessun attributo manifest principale in timer-manager.jar

--- script/distribute-time-manager.sh
+++ script/distribute-time-manager.sh
@@ -2,5 +2,5 @@ mvn clean assembly:assembly
 sleep 3
 rm ../dist -rf
 mkdir ../dist
-cp target/timer-manager.jar dist
+cp target/timer-manager-jar-with-dependencies.jar dist/timer-manager.jar
 cp src/main/resources/timer-manager.properties dist
