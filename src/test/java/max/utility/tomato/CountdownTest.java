ff1278686895f15b6bcb422f755a25ff00f37cce
max <massimiliano.parentini@gmail.com>
2012-11-07 20:43:23 +0100

    imp: exceptions thrown by executions of asynchronous tasks are logged.
    Here the logger is configured programmatically into the test method and
    is not used the junit @Rule annotation.

--- src/test/java/max/utility/tomato/CountdownTest.java
+++ src/test/java/max/utility/tomato/CountdownTest.java
@@ -1,57 +1,55 @@
 package max.utility.tomato;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.Callable;
 
-import max.test.logging.LogStub;
 import max.utility.tomato.aspects.task.LogExceptionAspect;
 import max.utility.tomato.tasks.ThorwUncheckedExceptionTask;
 
-import org.junit.Rule;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
 
 @ContextConfiguration
 public class CountdownTest extends AbstractJUnit4SpringContextTests {
+	private ListAppender<ILoggingEvent> listAppender;
 
 	public CountdownTest() {
 		PropertyLoader.loadFromClassPathAsInputStream("timer-manager.properties");
 	}
 
-	@Rule
-	public LogStub logStub = new LogStub() {
-		{
-			record(LogLevel.DEBUG);
-			recordLoggingForType(Countdown.class);
-			// togliendo i commenti a una delle righe successive, l'aspetto non
-			// viene eseguito.
-			// recordLoggingForType(ThorwUncheckedExceptionTask.class);
-			recordLoggingForType(LogExceptionAspect.class);
-		}
-	};
-
-	// qoogle: slf4j get appender
 	// http://stackoverflow.com/questions/3803184/setting-logback-appender-path-programmatically
-	// http://itsthecodestupid.blogspot.it/2011/02/programmatically-adding-appender-and.html
-	// http://www.slf4j.org/extensions.html#javaagent
-	// http://tux2323.blogspot.it/2011/06/test-logging-via-junit-rule.html
 	@Test
 	public void should_log_exception() throws InterruptedException {
+		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+		listAppender = new ListAppender<ILoggingEvent>();
+		listAppender.start();
+
+		// attach the rolling file appender to the logger of your choice
+		Logger logbackLogger = loggerContext.getLogger(LogExceptionAspect.class);
+		logbackLogger.addAppender(listAppender);
+
+		// OPTIONAL: print logback internal status messages
+		// StatusPrinter.print(loggerContext);
+
 		Countdown<Object> countdown = new Countdown<Object>();
 		Callable<Object> task = new ThorwUncheckedExceptionTask();
 		countdown.start(task);
 		Thread.sleep(6000);
-		ListAppender<ILoggingEvent> listAppender = logStub.getListAppender();
-		System.out.println(listAppender.list);
-		assertThat(logStub.size(), is(1));
-		assertTrue(logStub.containes("#catch_block#"));
-		assertTrue(logStub.catchException(ThorwUncheckedExceptionTask.BLA_BLA_BLA));
+		assertThat(listAppender.list.size(), is(2));
+		ILoggingEvent event = listAppender.list.get(1);
+		assertTrue(Main.CATCH_BLOCK.equals(event.getFormattedMessage()));
+		assertTrue(ThorwUncheckedExceptionTask.BLA_BLA_BLA.equals(event.getThrowableProxy().getMessage()));
+		assertNotNull(event.getThrowableProxy().getStackTraceElementProxyArray());
 	}
 }
