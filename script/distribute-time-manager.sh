bd992c2edd99bec9f3f834959020248aacadaa3b
max <massimiliano.parentini@gmail.com>
2012-11-08 18:37:10 +0100

    imp: create the zip file of the releaseble artifact

--- script/distribute-time-manager.sh
+++ script/distribute-time-manager.sh
@@ -2,7 +2,10 @@ export MAVEN_OPTS="-Xmx2024m -Xms1024m -javaagent:C:\Users\MAX\.m2\repository\or
 echo "MAVEN_OPTS:"  $MAVEN_OPTS
 mvn clean assembly:assembly  
 sleep 3
-rm ../dist -rf
-mkdir ../dist
-cp target/timer-manager-jar-with-dependencies.jar dist/timer-manager.jar
-cp src/main/resources/timer-manager.properties dist
+rm dist -rf
+mkdir -p dist/timer-manager
+cp target/timer-manager-jar-with-dependencies.jar dist/timer-manager/timer-manager.jar
+cp src/main/resources/timer-manager.properties dist/timer-manager
+cd dist
+sleep 2
+zip timer-manager timer-manager/*
