b1cc0b86ff8a3804c45023003a350ffab5c859a4
max <massimiliano.parentini@gmail.com>
2012-11-08 16:08:47 +0100

    imp: set MAVEN_OPTIONS for build executions

--- script/distribute-time-manager.sh
+++ script/distribute-time-manager.sh
@@ -1,3 +1,5 @@
+export MAVEN_OPTS="-Xmx2024m -Xms1024m -javaagent:C:\Users\MAX\.m2\repository\org\springframework\org.springframework.instrument\3.0.5.RELEASE\org.springframework.instrument-3.0.5.RELEASE.jar"
+echo "MAVEN_OPTS:"  $MAVEN_OPTS
 mvn clean assembly:assembly  
 sleep 3
 rm ../dist -rf
