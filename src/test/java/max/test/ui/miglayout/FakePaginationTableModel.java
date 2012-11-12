7132417c09f298c21ca740be8d48d3dac1540515
max <massimiliano.parentini@gmail.com>
2012-11-12 19:15:15 +0100

    imp [UI]: timer list panel

---
+++ src/test/java/max/test/ui/miglayout/FakePaginationTableModel.java
@@ -0,0 +1,85 @@
+package max.test.ui.miglayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import max.utility.tomato.domain.Tomato;
+import max.utility.tomato.domain.TomatoReview;
+
+public class FakePaginationTableModel extends AbstractTableModel {
+	private int startPosition;
+	private List<TomatoReview> theList;
+
+	public FakePaginationTableModel() {
+		theList = new ArrayList<TomatoReview>();
+		for (int j = 0; j < 20; j++) {
+			Tomato tomato = new Tomato("eeeeeeeeee -->   " + j);
+			TomatoReview tr = new TomatoReview(tomato, "afasdfasdf -->   " + j, "rrrrrrrrrrr -->   " + j);
+			theList.add(tr);
+		}
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 4;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		switch (column) {
+		case 0:
+			return "Data / Ora";
+		case 1:
+			return "Focus on";
+		case 2:
+			return "Cosa ho fatto realmente";
+		case 3:
+			return "Problemi incontrati";
+		default:
+			return "LAST NAME";
+		}
+	}
+
+	private List<TomatoReview> getItems(int rowIndex, int i) {
+
+		return theList;
+	}
+
+	@Override
+	public int getRowCount() {
+		return 20;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		if ((rowIndex >= startPosition) && (rowIndex < (startPosition + 100))) {
+		} else {
+			theList = getItems(rowIndex, rowIndex + 100);
+			startPosition = rowIndex;
+		}
+		TomatoReview tomatoReview = theList.get(rowIndex - startPosition);
+
+		Object toReturn = null;
+		switch (columnIndex) {
+		case 0:
+			toReturn = tomatoReview.getTomato().getStartTime().toString("dd/MM/yy - HH:mm");
+			break;
+		case 1:
+			toReturn = tomatoReview.getTomato().getFocusOn();
+			break;
+		case 2:
+			toReturn = tomatoReview.getReallyDone();
+			break;
+		case 3:
+			toReturn = tomatoReview.getProblemsRaised();
+			break;
+		default:
+			toReturn = tomatoReview.getId();
+
+		}
+		return toReturn;
+	}
+
+}
--- src/test/java/max/test/ui/miglayout/MLStartTimerWindow.java
+++ src/test/java/max/test/ui/miglayout/MLStartTimerWindow.java
@@ -4,15 +4,21 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.LayoutManager;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 
+import max.utility.tomato.gui.EvenOddRowCellRenderer;
 import net.miginfocom.swing.MigLayout;
 
 /**
@@ -22,6 +28,11 @@ import net.miginfocom.swing.MigLayout;
  * 
  */
 public class MLStartTimerWindow extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1171031715582612973L;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
@@ -42,16 +53,16 @@ public class MLStartTimerWindow extends JFrame {
 	}
 
 	private Component getPnlNewTimer() {
+
 		JPanel pnlNewTimer = new JPanel();
 		LayoutManager layout = new MigLayout("flowy", "fill,grow");
-		pnlNewTimer.setLayout(layout);
 		JTextArea taFocusOn = new JTextArea();
 		JScrollPane spFocusOn = new JScrollPane();
 		JLabel lblFocusOn = new JLabel("will focus on:");
+		JButton btnStart = new JButton();
 
-		JButton btnOk = new JButton();
-		JButton btnOkAndNew = new JButton();
-
+		pnlNewTimer.setLayout(layout);
+		btnStart.setText("inizia");
 		taFocusOn.setColumns(20);
 		taFocusOn.setForeground(new Color(0, 153, 0));
 		taFocusOn.setRows(5);
@@ -59,15 +70,53 @@ public class MLStartTimerWindow extends JFrame {
 		taFocusOn
 				.setText("descrivi cosa intendi fare nei prossimi 20 minuti... \n[Ctrl+tab] per togliere il \"focus\" dall' area di testo.\n max 250 caratteri");
 		taFocusOn
-				.setToolTipText("descrivi cosa intendi fare nei prossimi 20 minuti... \n[Ctrl+tab] per togliere il \"focus\" dall' area di testo");
+				.setToolTipText("cosa intendi fare nei prossimi 20 minuti? \n[Ctrl+tab] per togliere il \"focus\" dall' area di testo");
 		spFocusOn.setViewportView(taFocusOn);
-		taFocusOn.getAccessibleContext().setAccessibleName("ta_focusOn");
+		taFocusOn.getAccessibleContext().setAccessibleName("taFocusOn");
 
 		pnlNewTimer.add(lblFocusOn);
 		pnlNewTimer.add(spFocusOn, "growx");
-		pnlNewTimer.add(btnOk);
-		pnlNewTimer.add(btnOkAndNew);
+		pnlNewTimer.add(btnStart);
+
 		return pnlNewTimer;
+	}
+
+	private Component getPnlTimersList() {
+		JPanel pnlTimersList = new JPanel();
+		JScrollPane spTimerList = new JScrollPane();
+		final JTable jTable1 = new JTable();
+		spTimerList.setViewportView(jTable1);
+		jTable1.setRowHeight(40);
+		jTable1.setModel(new FakePaginationTableModel());
+		jTable1.setDefaultRenderer(Object.class, new EvenOddRowCellRenderer());
+		jTable1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 1) {
+					JTable target = (JTable) e.getSource();
+					int row = target.getSelectedRow();
+					int column = target.getSelectedColumn();
+					JTextArea ta = new JTextArea();
+					ta.setLineWrap(true);
+					int constant = 400;
+					int constant1 = 300;
+					ta.setSize(constant, constant1);
+					ta.setEditable(false);
+					String text = (String) jTable1.getModel().getValueAt(row, column);
+					ta.setText(text);
+					ta.setLineWrap(true);
+					JScrollPane sp = new JScrollPane();
+					sp.setViewportView(ta);
+					sp.setSize(constant, constant1);
+					sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+					sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+					JOptionPane.showMessageDialog(jTable1, sp);
+				}
+
+			}
+		});
+		pnlTimersList.add(spTimerList);
+		return pnlTimersList;
 	}
 
 	private void init() {
@@ -76,9 +125,8 @@ public class MLStartTimerWindow extends JFrame {
 
 		JTabbedPane tabTimers = new JTabbedPane();
 
-		JPanel pnlTimersList = new JPanel();
 		tabTimers.addTab("nuovo timer", getPnlNewTimer());
-		tabTimers.addTab("lista dei timer", pnlTimersList);
+		tabTimers.addTab("lista dei timer", getPnlTimersList());
 		getContentPane().add(tabTimers);
 	}
 }
